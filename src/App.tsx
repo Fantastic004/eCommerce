d0178c9b47c93781bda49b944351de6f6a57b0ec
Praveen Pk <155797643+Pkpraveen20@users.noreply.github.com>
2025-04-29 17:26:29 +0530

    loginpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import Login from "./components/Login";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGoogle,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import OrderConfirmation from "./components/Ordersucefull";
+
+library.add(faGoogle, faFacebook, faInstagram);
 
 function App() {
-
   return (
     <>
-      
+      {/* <Login /> */}
+      <OrderConfirmation />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
